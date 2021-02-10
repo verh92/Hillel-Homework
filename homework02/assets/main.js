dc5d439b319bad0a948ea483d5db611dc90ac476
vverh92 <vverh92@gmail.com>
2021-02-10 23:44:28 +0200

    hw02

---
+++ homework02/assets/main.js
@@ -0,0 +1,13 @@
+let num1 = +prompt('Enter the first number');
+let num2 = +prompt('Enter the second number');
+
+let res = ` 
+    Calculations are finished!\n
+    Sum: ${num1} + ${num2} = ${num1 + num2}\n
+    Diff: ${num1} - ${num2} = ${num1 - num2}\n
+    Mult: ${num1} * ${num2} = ${num1 * num2}\n
+    Div: ${num1} / ${num2} = ${num1 / num2}\n
+`;
+
+alert(res);
+console.log(res);
